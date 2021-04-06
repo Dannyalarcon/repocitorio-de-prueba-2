935652a3f834667648e7fd5fc5c07aa9ebaebfe0
Danny_Alarcon_7 <dannyalarcon417@gmail.com>
2021-04-05 18:16:26 -0600

    1

---
+++ PRUEBA DE REPOCITORIO/tarea1/conexion.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+    $host = '127.0.0.1:3308';
+    $user = 'root';
+    $password = '';
+    $db = 'tarea1';
+
+    $conection = @mysqli_connect($host, $user, $password, $db);
+
+    //mysqli_close($conection);
+
+    if(!$conection){
+        echo "Error en la conexion";
+    }
+
+?>
